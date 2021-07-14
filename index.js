ae4b10024d7549dbe98d08b211368fe10571b089
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-07-13 21:12:57 -0400

    commit tree ece4036a95a38ed0cab86102a60809d5eddabd42

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(array, callbackFn) {
+        for (const e of array) {
+          callbackFn(e);
+        }
     },
 
     map: function() {
